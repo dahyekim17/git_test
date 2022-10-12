662248fd00febff2f699f7dfde6e4f5b2da6a22a
dahyekim17 <110885336+dahyekim17@users.noreply.github.com>
2022-10-12 20:32:01 +0900

    Add files via upload

---
+++ js/custom_tab1.js
@@ -0,0 +1,24 @@
+$(function () {
+  $(".tabcontent1 > div").hide();
+  $(".tabnav1 a")
+    .click(function () {
+      $(".tabcontent1 > div").hide().filter(this.hash).fadeIn();
+      $(".tabnav1 a").removeClass("active");
+      $(this).addClass("active");
+      return false;
+    })
+    .filter(":eq(0)")
+    .click();
+
+
+    $(".tabcontent2 > div").hide();
+    $(".tabnav2 a")
+      .click(function () {
+        $(".tabcontent2 > div").hide().filter(this.hash).fadeIn();
+        $(".tabnav2 a").removeClass("active");
+        $(this).addClass("active");
+        return false;
+      })
+      .filter(":eq(0)")
+      .click();
+});
